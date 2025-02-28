dd61c73b5e34faacec89270f030f8a2dad9d1318
noisefigure203 <jinlongchen203@gmail.com>
2025-02-28 11:11:59 +0800

    test1

--- cpu-api/p1.c
+++ cpu-api/p1.c
@@ -1,20 +1,20 @@
-//#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 int
 main(int argc, char *argv[])
 {
-    printf("hello world (pid:%d)\n", (int) getpid()); /* printf() function is defined in 
-                                                        getpid() function is defined in unistd.h 
+    printf("hello world (pid:%d)\n", (int) getpid()); /* printf() function is defined in stdio.h
+                                                         getpid() function is defined in unistd.h 
                                                             */
     
     int rc = fork();  /*fork() function is defined in unistd.h*/
     
     if (rc < 0) {
         // fork failed; exit
-        fprintf(stderr, "fork failed\n");
-        exit(1);
+        fprintf(stderr, "fork failed\n"); /* stderr is defined in stdio.h */
+        exit(1); /* exit() is define in stdio.h */
     } else if (rc == 0) {
         // child (new process)
         printf("hello, I am child (pid:%d)\n", (int) getpid());
